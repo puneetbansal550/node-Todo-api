13596efd26abf85a7711037fdcdfe76d23522ac1
puneetBansal <puneetbansalsb@gmail.com>
2018-08-13 05:03:24 +0530

    setting production DB URI 2

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,14 @@
 var env = process.env.NODE_ENV || 'develpoment';
-var config = require('./config.json');
-var envConfig = config[env];
+
+
 if(env === 'test' || env === 'develpoment'){
+  var config = require('./config.json');
+  var envConfig = config[env];
   Object.keys(envConfig).forEach((key) => {
     process.env[key] = envConfig[key];
   });
 }
+
 else{
   process.env.MONGODB_URI = "mongodb://puneet:puneet123@ds253821.mlab.com:53821/todoapp_puneet";
 }
